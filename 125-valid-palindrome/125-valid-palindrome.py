13277b667e431ec0d10bcb96e97a2191dfc4a3a1
keanureano <112625935+keanureano@users.noreply.github.com>
2023-04-25 23:39:29 +0800

    Time: 53 ms (38.65%), Space: 14.5 MB (94.64%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -3,10 +3,10 @@ class Solution:
         left, right = 0, len(s) - 1
         
         while left < right:
-            while left < right and not self.isAlphaNum(s[left]):
+            while left < right and not s[left].isalnum():
                 left += 1
                 
-            while left < right and not self.isAlphaNum(s[right]):
+            while left < right and not s[right].isalnum():
                 right -= 1
                 
             if s[left].lower() != s[right].lower():
@@ -16,10 +16,4 @@ class Solution:
             right -= 1
             
         return True
-    
-    def isAlphaNum(self, char):
-        return ((ord("A") <= ord(char) <= ord("Z")) or
-                (ord("a") <= ord(char) <= ord("z")) or
-                (ord("0") <= ord(char) <= ord("9")))
-        
         
